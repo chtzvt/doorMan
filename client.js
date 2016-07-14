1755e3280c083883dca90dbd0643416ec8a4fce6
Charlton Trezevant <ct@ctis.me>
2016-07-14 14:10:07 -0400

    Initial commit

---
+++ client.js
@@ -0,0 +1,152 @@
+var req = require('requests');
+var dsyc = require('deasync');
+
+function DoorController(config) {
+    this.controller_host = config.host;
+    this.statusMonitors = {};
+}
+
+DoorController.prototype.open = function(doorId) {
+    this.sendCommand('open', doorId);
+}
+
+DoorController.prototype.close = function(doorId) {
+    this.sendCommand('close', doorId);
+}
+
+DoorController.prototype.cycle = function(doorId) {
+    this.sendCommand('cycle', doorId);
+}
+
+DoorController.prototype.lockout = function(doorId) {
+    this.sendCommand('lockout', doorId);
+}
+
+// Synchronously queries API for state of specified door.
+// State: true/false -> open/closed
+DoorController.prototype.getStateSync = function(doorId) {
+    var state;
+    request.get(this.controller_host + '/get/state?id=' + doorId, function(error, response, body) {
+        if (error || response.statusCode != 200) {
+            console.log('[doorControl] QUERY STATE id: ' + this.id + ' got status code ' + response.statusCode + ' and exited with error: ' + error);
+            return;
+        }
+
+        body = JSON.parse(body);
+
+        if (!body.error)
+            state = body.state;
+        else
+            console.log('[doorControl] QUERY STATE id: ' + this.id + ' failed with error: ' + JSON.stringify(body));
+    }.bind({
+        id: doorId
+    }));
+
+    while (state === undefined) {
+        dsyc.runLoopOnce();
+    }
+
+    return state;
+}
+
+// Synchronously queries API for IDs of available doors.
+DoorController.prototype.enumerateDoorsSync = function() {
+    var doors;
+    request.get(this.controller_host + '/get/doors_list', function(error, response, body) {
+        if (error || response.statusCode != 200) {
+            console.log('[doorControl] ENUMERATE DOORS: got status code ' + response.statusCode + ' and exited with error: ' + error);
+            return;
+        }
+
+        body = JSON.parse(body);
+
+        if (!body.error)
+            doors = body.doors;
+        else
+            console.log('[doorControl] ENUMERATE DOORS: failed with error: ' + JSON.stringify(body));
+    });
+
+    while (doors === undefined) {
+        dsyc.runLoopOnce();
+    }
+
+    return doors;
+}
+
+// Monitors the status of the specified door at the specified interval, returns event emitter.
+DoorController.prototype.monitorStatus = function(doorId, timeout) {
+    var emitter = new(require('events').EventEmitter);
+
+    this.statusMonitors[doorId] = {
+        monitor: emitter,
+        monitorId: 0,
+        statusMonitorInterval: timeout,
+        lastKnownState: this.getStateSync(doorId)
+    }
+
+    this.statusMonitors[doorId].monitorId = setInterval(watchState.bind(doorId, this.statusMonitors[doorId]), timeout);
+
+    return this.doorId.monitor;
+}
+
+// Destroys status monitor of specified ID
+DoorController.prototype.destroyMonitor = function(monitorId) {
+    return clearInterval(monitorId);
+}
+
+var watchState = function(doorId, doorInfo) {
+    request.get(this.controller_host + '/get/state?id=' + doorId, function(error, response, body) {
+        if (error || response.statusCode != 200) {
+            console.log('[doorControl] QUERY STATE id: ' + this.id + ' got status code ' + response.statusCode + ' and exited with error: ' + error);
+            return;
+        }
+
+        body = JSON.parse(body);
+
+        // API errors will return an error key in responses and set state to "error"
+        if (body.error) {
+            em.emit("error");
+            dc.lastKnownState = "error";
+            return;
+        }
+
+        // API Lockout will emit a lockout event.
+        if (body.lockout) {
+            em.emit("lockout");
+            dc.lastKnownState = "lockout";
+            return;
+        }
+
+        // if current state is open and last known state was closed then emit open event
+        if (body.state === true && lastState === !body.state) {
+            em.emit("open");
+            dc.lastKnownState = true;
+            return;
+        }
+
+        // if current state is closed and last known state was open then emit close event
+        if (body.state === false && lastState === !body.state) {
+            em.emit("close");
+            dc.lastKnownState = false;
+            return;
+        }
+
+    }.bind({
+        em: doorInfo.monitor,
+        lastState: doorInfo.lastKnownState
+    }));
+}
+
+// Sends open/close/lockout/etc command to API for specified door. Performs no success checks or error handling.
+DoorController.prototype.sendCommand = function(command, doorId) {
+    request(this.controller_host + '/set/' + cmd + '?id=' + doorId, function(error, response, body) {
+        if (error || response.statusCode != 200) {
+            console.log('[doorControl] OPEN id: ' + this.id + ' got status code ' + response.statusCode + ' and exited with error: ' + error);
+            return;
+        }
+    }.bind({
+        id: doorId
+    }));
+}
+
+module.exports = DoorController;
---
+++ server.js
@@ -0,0 +1,144 @@
+var gpio = require('chip-gpio').Gpio;
+var sleep = require('sleep').sleep;
+var http = require('http');
+var url = require('url');
+
+var CONFIG = {
+    doors: [ // Object position in array corresponds to door ID.
+        {
+            id: 0,
+            lift_pin: 0,
+            sensor_pin: 0,
+            lockout: false,
+            human_name: "Left Door",
+            lift_ctl: '',
+            sensor_ctl: ''
+        }
+    ],
+    http_port: 8080
+};
+
+parseConfig();
+
+http.createServer(function(req, res) {
+
+    if (req.method === "GET") {
+        console.log("Called " + req.url + " for door number: " + call.query.id);
+
+        var call = url.parse(req.url, true);
+
+        if (CONFIG.doors[call.query.id] === undefined) {
+            res.writeHead(200);
+            res.end(JSON.stringify({
+                error: "no such door"
+            }));
+        }
+
+        switch (call.pathname) {
+            case "/get/state":
+                res.writeHead(200);
+                var state = {
+                    state: CONFIG.doors[call.query.id].lift_ctl.read(),
+                    lockout: CONFIG.doors[call.query.id].lockout
+                };
+                res.end(JSON.stringify(state));
+                break;
+
+            case "/get/list":
+                res.writeHead(200);
+                var list = strip_gpioCtl(CONFIG.doors);
+                res.end(JSON.stringify({
+                    list: list
+                }));
+                break;
+
+            case "/set/open": // Below may need to be reordered depending on whether this function is exited after res.end is called. 
+                res.writeHead(200);
+                res.end(JSON.stringify({
+                    command_sent: true
+                }));
+                tripCircuit(call.query.id, 1, false);
+                break;
+
+            case "/set/close": // Below may need to be reordered depending on whether this function is exited after res.end is called.
+                res.writeHead(200);
+                res.end(JSON.stringify({
+                    command_sent: true
+                }));
+                tripCircuit(call.query.id, 0, false);
+                break;
+
+            case "/set/cycle": // Below may need to be reordered depending on whether this function is exited after res.end is called.
+                res.writeHead(200);
+                res.end(JSON.stringify({
+                    command_sent: true
+                }));
+                tripCircuit(call.query.id, null, true); // Cycle door state by sending command regardless of sensor reading
+                break;
+
+            case "/set/lockout": // Below may need to be reordered depending on whether this function is exited after res.end is called.
+                res.writeHead(200);
+                res.end(JSON.stringify({
+                    command_sent: true
+                }));
+                tripCircuit(call.query.id, 0, false); // Close door.
+                CONFIG.doors[call.query.id].lockout = true; // set lockout flag on door.
+                break;
+
+            default:
+                res.writeHead(400);
+                res.end(JSON.stringify({
+                    error: "method not implemented"
+                }));
+        }
+
+        console.log("Completed method " + req.url + " for door number: " + call.query.id);
+
+    } else {
+        res.writeHead(400);
+        res.end(JSON.stringify({
+            error: "malformed request"
+        }));
+    }
+
+}.bind({
+    CONFIG: CONFIG
+})).listen(CONFIG.http_port);
+
+// Parses configuration object, configures GPIO control for each door programmatically. 
+function parseConfig() {
+    // Instantiate GPIO controller for each door and attach this to the door object.
+    for (var i = 0; i < CONFIG.doors.length; i++) {
+        CONFIG.doors[i].lift_ctl = new Gpio(CONFIG.doors[i].lift_pin, 'out');
+        CONFIG.doors[i].sensor_ctl = new Gpio(CONFIG.doors[i].sensor_pin, 'in');
+    }
+}
+
+// Creates a copy of the door configuration and strips the GPIO control methods from it, as we do not want to include these
+// in the JSON response.
+function strip_gpioCtl(doors) {
+    var strippedList = [];
+    for (var i = 0; i < doors.length; i++) {
+        var newDoor = Object.create(doors[i]);
+        delete newDoor.lift_ctl;
+        delete newDoor.sensor_ctl;
+        strippedList[i] = newDoor;
+    }
+    return strippedList;
+}
+
+// Trips circuit based on optional initial condition of door.
+// Params:
+//	id - id of door to open
+// 	initialState - initial state the door MUST be in in order for command to be sent.
+// 	bypass - bypass state check (optional)
+function tripCircuit(id, initialState, bypass) {
+    if (bypass === true || CONFIG.doors[call.query.id].sensor_ctl.read() == initialState) {
+        // Close relay
+        CONFIG.doors[id].lift_ctl.write(1);
+        // Sleep for 1 second
+        sleep(1000);
+        // Open relay
+        CONFIG.doors[id].lift_ctl.write(0);
+    }
+}
