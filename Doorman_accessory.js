5e3093cd036983994eb0a1a91ea35a9354f7733e
Charlton Trezevant <ct@ctis.me>
2016-07-23 14:21:15 -0400

    Add real-time door status monitoring, minor aesthetic improvements

--- Doorman_accessory.js
+++ Doorman_accessory.js
@@ -3,26 +3,43 @@ var Service = require('../').Service;
 var Characteristic = require('../').Characteristic;
 var uuid = require('../').uuid;
 
-// Interface with the garage door opener API
+// Client Library interfaces with DoorMan server.
 var DoorController = require("../lib/Doorman_DoorCtl.js");
+
 // Default API server is localhost on port 8080, but this can be any server. For example, you might run
 // the HAP server on a different networked device than the C.H.I.P. itself.
 var dctl = new DoorController({host:'http://localhost:8080', api_key: 'default'});
+
 // By default, we use door ID 0. If you want other doors, their IDs can be retrieved using the dctl.enumerateDoorsSync() method.
 var doorTarget = 0;
 
-// Add a few methods that are useful to HAP 
+// Add a few methods that are useful to HAP
 dctl.opened = function() {
-	var state = dctl.getStateSync(doorTarget) == 0 ? true : false;
-	console.log("DOORMAN_INFO: Query door state: " + ((state == true) ? "open" : "closed"));
-	return state;
-};
-dctl.identify = function() {
-    console.log("DOORMAN_INFO: Identified.");
-//    dctl.cycle();
+    var state = dctl.getStateSync(doorTarget) === 0 ? true : false;
+    //console.log("DOORMAN_INFO: Query door state: " + ((state === true) ? "open" : "closed"));
+    return state;
 };
 
-console.log("DOORMAN_INFO: Initial door state is " + ((dctl.opened() == true) ? "open" : "closed"));
+dctl.identify = function() {
+    console.log("DOORMAN_INFO: Identified.");
+    // You could blink an LED or something here.
+};
+
+// Override included status monitor from client lib
+dctl.monitorStatus = function() {
+    if (dctl.opened()) {
+        garage
+            .getService(Service.GarageDoorOpener)
+            .setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.OPEN);
+    } else {
+        garage
+            .getService(Service.GarageDoorOpener)
+            .setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.CLOSED);
+    }
+};
+setInterval(dctl.monitorStatus, 1000);
+
+console.log("DOORMAN_INFO: Initial door state is " + ((dctl.opened() === true) ? "open" : "closed"));
 
 var garageUUID = uuid.generate('hap-nodejs:accessories:garage');
 var garage = exports.accessory = new Accessory('Garage Door', garageUUID);
@@ -50,18 +67,10 @@ garage
     if (value == Characteristic.TargetDoorState.CLOSED) {
       dctl.close(doorTarget);
       callback();
-
-      garage
-        .getService(Service.GarageDoorOpener)
-        .setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.CLOSED);
     }
     else if (value == Characteristic.TargetDoorState.OPEN) {
       dctl.open(doorTarget);
       callback();
-
-      garage
-        .getService(Service.GarageDoorOpener)
-        .setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.OPEN);
     }
   });
 
